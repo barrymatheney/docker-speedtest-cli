ee9bc8573f17c80a342e8f9878be59ac18dcbe15
Barry Matheney <barry_matheney_msdn@outlook.com>
2020-06-01 21:19:22 -0400

    got the basic script working to deconstruct the output for saving in the other file

--- execspeedtest.sh
+++ execspeedtest.sh
@@ -2,9 +2,52 @@
 !#/bin/bash
 
 # execspeedtest.sh
-$filename="/tmp/testresults.txt"
-$hostfilename="/output/speedtesthistory.txt"
+filename="/tmp/testresults.txt"
 
-/usr/local/bin/speedtest-cli > "$filename"
-cat "$filename" | grep 
+filename="./sample.txt"
 
+hostfilename="/output/speedtesthistory.txt"
+pipechar="|"
+################################/usr/local/bin/speedtest-cli > "$filename"
+lineout=$(date +%Y%m%d_%H%M%S)
+echo $lineout
+
+#get the source IP from the output
+val=""
+val=$(grep  'Testing from ' $filename) 
+val=$(echo $val | grep -Po '(?<=\().*(?=\))')
+lineout=$lineout$pipechar$val
+
+#get the destination host info provided
+val=""
+srch='Hosted by '
+val=$(grep  "$srch" $filename) 
+charstosave=`expr ${#val} - ${#srch}`
+echo $charstosave
+val=${val: -$charstosave}
+echo $val
+lineout=$lineout$pipechar$val
+echo $lineout
+
+
+#get the Down speed
+val=""
+srch='Download: '
+val=$(grep  "$srch" $filename) 
+charstosave=`expr ${#val} - ${#srch}`
+echo $charstosave
+val=${val: -$charstosave}
+echo $val
+lineout=$lineout$pipechar$val
+echo $lineout
+
+#get the u[] speed
+val=""
+srch='Upload: '
+val=$(grep  "$srch" $filename) 
+charstosave=`expr ${#val} - ${#srch}`
+echo $charstosave
+val=${val: -$charstosave}
+echo $val
+lineout=$lineout$pipechar$val
+echo $lineout
